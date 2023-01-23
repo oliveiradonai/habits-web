be4bdd0fd9f6b4d236121e314cc610510b996c07
oliveiradonai <adonai.fig@gmail.com>
2023-01-23 15:44:10 -0300

    GET API data

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import './styles/global.css';
-
-// import { Habit } from "./components/Habit"
+import './lib/dayjs';
 import { Header } from './components/Header';
 import { SummaryTable } from './components/SummaryTable';
+
+// import { Habit } from "./components/Habit"
 
 export function App() {
   return (
